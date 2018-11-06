61e2b60f17a6f2581e4b22f0eabcca0a7382d324
Matheus Michel <matmic08@gmail.com>
2018-11-06 18:14:05 -0200

    Atualiza a capa do site e a página de login.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,10 +4,22 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Bem vindo ao <i>Portal de Publicações do Programa Mais Médicos</i></h1>
+<h1><i>Portal de Publicações da Rede Observatório Programa Mais Médicos</i></h1>
 
+<p>
+	Bem-vindo ao <b>Portal de Publicações da Rede Observatório Programa Mais Médicos</b>. Aqui
+	você vai encontrar o que foi publicado em revistas e periódicos acadêmicos sobre temas do
+	Programa Mais Médicos e outros relacionados a eles. Clique no menu ao lado e comece a
+	navegar.
+</p
 <div class="container h-100">
 	<div class="row h-100 justify-content-center align-items-center">
-		<img class="img-fluid" src="<?php echo Yii::app()->baseUrl; ?>/images/logo-mais-medicos.jpg" alt="">
+		<img class="img-fluid" src="<?php echo Yii::app()->baseUrl; ?>/images/capa.jpg" alt="">
 	</div>
+</div>
+
+<div class="text-center">
+	<img width='200px' src="<?php echo Yii::app()->baseUrl; ?>/images/logo-ms.png" class="img-fluid">
+	<img width='200px' src="<?php echo Yii::app()->baseUrl; ?>/images/logo-mais-medicos2.jpg" class="img-fluid">
+	<img width='200px' src="<?php echo Yii::app()->baseUrl; ?>/images/logo-ufrgs.png" class="img-fluid">
 </div>
--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -1,47 +1,33 @@
-<div class="peers ai-s fxw-nw h-100vh">
-	<div class="d-n@sm- peer peer-greed h-100 pos-r bgr-n bgpX-c bgpY-c bgsz-cv" style="background-image:url(<?php echo Yii::app()->baseUrl; ?>/images/bg.jpg)">
-		<div class="pos-a centerXY">
-			<div class="bgc-white bdrs-50p pos-r" style="width:120px;height:120px"><img class="pos-a centerXY" src="<?php echo Yii::app()->baseUrl; ?>/images/logo.png" alt=""></div>
-		</div>
+<h4 class="c-grey-900">Login</h4>
+	<div class="mB-40">
+		<span style="color: red;">O Login é restrito aos administradores do sistema</span>
 	</div>
-	<div class="col-12 col-md-4 peer pX-40 pY-80 h-100 bgc-white scrollable pos-r ps" style="min-width:320px">
-		<h4 class="fw-300 c-grey-900">Login</h4>
-		<span style="color: red;">Login restrito para administradores do sistema</span>
-		<?php
-			echo CHtml::beginForm(Yii::app()->createUrl('site/login'), 'POST', array('id'=>'needs-validation', 'class'=>'container', 'noValidate'=>""));
-			echo '<div class="form-group row">';
-				echo CHtml::label('E-mail: ', 'lblEmail', array('class'=>'text-normal text-dark'));
-				echo '<div class="col-sm-10">';
-					echo CHtml::activeTextField($usuario, 'EmailUsuario', array('required'=>true, 'class'=>'form-control'));
-					echo '<div class="invalid-feedback">Por favor, insira o email.</div>';
-				echo '</div>';
+	<?php
+		echo CHtml::beginForm(Yii::app()->createUrl('site/login'), 'POST', array('id'=>'needs-validation', 'class'=>'container', 'noValidate'=>""));
+		echo '<div class="form-group row">';
+			echo CHtml::label('E-mail: ', 'lblEmail', array('class'=>'col-sm-2 col-form-label alinharDireita'));
+			echo '<div class="col-sm-10">';
+				echo CHtml::activeTextField($usuario, 'EmailUsuario', array('required'=>true, 'class'=>'form-control'));
+				echo '<div class="invalid-feedback">Por favor, insira o email.</div>';
 			echo '</div>';
-			
-			echo '<div class="form-group row">';
-				echo CHtml::label('Senha: ', 'lblEmail', array('class'=>'text-normal text-dark'));
-				echo '<div class="col-sm-10">';
-					echo CHtml::activePasswordField($usuario, 'SenhaUsuario', array('required'=>true, 'class'=>'form-control'));
-					echo '<div class="invalid-feedback">Por favor, insira a senha.</div>';
-				echo '</div>';
+		echo '</div>';
+		
+		echo '<div class="form-group row">';
+			echo CHtml::label('Senha: ', 'lblEmail', array('class'=>'col-sm-2 col-form-label alinharDireita'));
+			echo '<div class="col-sm-10">';
+				echo CHtml::activePasswordField($usuario, 'SenhaUsuario', array('required'=>true, 'class'=>'form-control'));
+				echo '<div class="invalid-feedback">Por favor, insira a senha.</div>';
 			echo '</div>';
-			
-			echo '<div class="form-group row">';
-				echo '<div class="col-sm-12">';
-					echo CHtml::submitButton('Login', array('class'=>"btn btn-primary"));
-				echo '</div>';
+		echo '</div>';
+		
+		echo '<div class="text-center form-group row">';
+			echo '<div class="col-sm-12">';
+				echo CHtml::submitButton('Login', array('class'=>"btn btn-primary"));
 			echo '</div>';
-	
-			echo CHtml::endForm();
-		?>
-		<div class="ps__rail-x" style="left: 0px; bottom: 0px;">
-			<div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
-		</div>
-		<div class="ps__rail-y" style="top: 0px; right: 0px;">
-			<div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div>
-		</div>
-	</div>
-</div>
+		echo '</div>';
 
+		echo CHtml::endForm();
+	?>
 
 <script>
     !function() {
