c1c48cc768e0673624b99a4df5c8081473363ff2
Matheus Michel <matmic08@gmail.com>
2018-11-07 13:49:13 -0200

    Permite edição e exclusão de palavras-chave.

--- protected/controllers/PalavraController.php
+++ protected/controllers/PalavraController.php
@@ -14,4 +14,84 @@ class PalavraController extends BaseController
 		
 		$this->render('listar', array('dataProvider'=>$dataProvider));
 	}
+	
+	public function actionFormulario()
+	{
+		// Carrega formulário com os dados
+		if (isset($_GET['CodPalavra']) && !empty($_GET['CodPalavra']))
+		{
+			$palavra = Palavra::model()->findByPk($_GET['CodPalavra']);
+			
+			if (empty($palavra))
+			{
+				Yii::app()->user->setFlash('danger', 'Não foi encontrada uma palavra-chave válida!');
+				$this->redirect(array('palavra/listar'));
+			}
+			else
+				$this->render('formulario', array('palavra'=>$palavra));
+		}
+		else
+		{
+			// Atualização ou Novo
+			if (isset($_POST['Palavra']))
+			{
+				if (!empty($_POST['Palavra']['CodPalavra']))
+				{
+					$palavra = Palavra::model()->findByPk($_POST['Palavra']['CodPalavra']);
+					if (empty($palavra))
+					{
+						Yii::app()->user->setFlash('danger', 'Não foi encontrada uma palavra-chave válida!');
+						$this->redirect(array('palavra/listar'));
+					}
+				}
+				else
+					$palavra = new Palavra();
+				
+				$palavra->attributes = $_POST['Palavra'];
+
+				if (!$palavra->save())
+					Yii::app()->user->setFlash('danger', 'Não foi possível salvar a palavra-chave!');
+				else
+					
+					Yii::app()->user->setFlash('success', 'Palavra-chave salva com sucesso!');
+
+				$this->redirect(array('palavra/listar'));
+			}
+			// Carrega formulário em branco
+			else
+			{
+				$palavra = new Palavra();
+				$this->render('formulario', array('palavra'=>$palavra));
+			}
+		}
+	}
+		
+	public function actionRemover()
+	{
+		if (isset($_GET['CodPalavra']) && !empty($_GET['CodPalavra']))
+		{
+			$palavra = Palavra::model()->findByPk($_GET['CodPalavra']);
+			
+			if (empty($palavra))
+			{
+				Yii::app()->user->setFlash('danger', 'Palavra-chave não encontrada!');
+				$this->redirect('listar');
+			}
+			
+			try
+			{
+				$palavra->delete();
+				echo 'Palavra-chave excluída com sucesso!';
+			}
+			catch (Exception $e)
+			{
+				echo 'Existem artigos vinculados a essa palavra-chave, não é possível excluí-la!';
+			}
+		}
+		else
+		{
+			Yii::app()->user->setFlash('danger', 'Palavra-chave não encontrada!');
+			$this->redirect('listar');
+		}
+	}
 }
--- protected/views/palavra/listar.php
+++ protected/views/palavra/listar.php
@@ -3,15 +3,67 @@
 	echo '<p>Para visualizar as palavras-chave mais utilizadas, clique <a href="' . Yii::app()->createUrl('grafico/palavra', array('Numero'=>10)) . '">aqui</a>!</p>';
 
 	echo '<div class="table-responsive">';
-		$this->widget('zii.widgets.grid.CGridView', array(
-			'dataProvider'=>$dataProvider,
-			'itemsCssClass'=>'table table-striped table-bordered table-condensed',
-			'columns'=>array(
-				array(
-					'header'=>'Palavra',
-					'value'=>'$data->NomePalavra',
+		if (!Yii::app()->user->isGuest)
+		{
+			$this->widget('zii.widgets.grid.CGridView', array(
+				'id'=>'gridPalavras',
+				'dataProvider'=>$dataProvider,
+				'itemsCssClass'=>'table table-striped table-bordered table-condensed',
+				'columns'=>array(
+					array(
+						'header'=>'Palavra',
+						'value'=>'$data->NomePalavra',
+					),
+					array(
+						'htmlOptions'=>array('style'=>"width: 30px; text-align: center;"),
+						'header'=>'Visualizar',
+						'class'=>'CButtonColumn',
+						'template'=>'{update} {deletar}',
+						'buttons'=>array(
+							'update' => array
+							(
+								'label'=>'Editar',
+								'imageUrl'=>Yii::app()->request->baseUrl.'/images/editar.png',
+								'url'=>'Yii::app()->createUrl("palavra/formulario", array("CodPalavra"=>"$data->CodPalavra"))',
+							),
+							'deletar' => array
+							(
+								'label'=>'Deletar',
+								'imageUrl'=>Yii::app()->request->baseUrl.'/images/remover.png',
+								'url'=>'Yii::app()->createUrl("palavra/remover", array("CodPalavra"=>"$data->CodPalavra"))',
+								'click'=>"function(){
+									if(confirm('Você tem certeza que deseja excluir essa palavra-chave?'))
+									{
+										$.ajax({
+											type:'POST',
+											url:$(this).attr('href'),
+											success : function(retorno){ 
+												alert(retorno);
+												$.fn.yiiGridView.update('gridPalavras');
+											},
+										});
+									}
+								return false;
+								}",
+							),
+						),
+					),
 				),
-			),
-		));
+			));
+		}
+		else
+		{
+			$this->widget('zii.widgets.grid.CGridView', array(
+				'id'=>'gridPalavras',
+				'dataProvider'=>$dataProvider,
+				'itemsCssClass'=>'table table-striped table-bordered table-condensed',
+				'columns'=>array(
+					array(
+						'header'=>'Palavra',
+						'value'=>'$data->NomePalavra',
+					),
+				),
+			));
+		}
 	echo '</div>';
 ?>
